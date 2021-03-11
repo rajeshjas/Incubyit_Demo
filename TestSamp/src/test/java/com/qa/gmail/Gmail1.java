00f07425a94cea6f049ec90fb8ee11491ee896f8
rajeshjas <rajeshjasan08@gmail.com>
2021-03-11 17:54:32 +0530

    second commit

--- TestSamp/src/test/java/com/qa/gmail/Gmail1.java
+++ TestSamp/src/test/java/com/qa/gmail/Gmail1.java
@@ -1,5 +1,6 @@
 package com.qa.gmail;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -10,7 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Gmail1 {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		System.setProperty("webdriver.chrome.driver","F:\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -25,7 +26,11 @@ public class Gmail1 {
 		wait.until(ExpectedConditions.elementToBeClickable(password));
 		password.sendKeys("Vignesh@12345");
 		driver.findElement(By.id("passwordNext")).click();
-
-
+        driver.findElement(By.xpath("//div[@style='user-select: none']")).click();
+        driver.findElement(By.xpath("//*[@id=':th']")).click();
+        Runtime.getRuntime().exec("E:\\AutoIT\\FileUpload.exe");
+        driver.findElement(By.xpath("//*[@tabindex='1'][@class='T-I J-J5-Ji aoO v7 T-I-atl L3']")).click();
+        driver.close();
 	}
+	
 }
